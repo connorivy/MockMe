b16da37f3a5dc523aaa689825b7a17c424ea00b7
Connor Ivy <connor@speckle.systems>
2024-11-11 10:34:41 -0600

    add another inline test

--- test/MockMe.Tests/InliningTests.cs
+++ test/MockMe.Tests/InliningTests.cs
@@ -4,6 +4,34 @@ using Xunit;
 
 namespace MockMe.Tests
 {
+    public class PotentiallyInlinedMethods
+    {
+        public int AddNormal(int x, int y) => x + y;
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public int AddNoInline(int x, int y) => x + y;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int AddAggressiveInline(int x, int y) => x + y;
+    }
+
+    public class InlineTestCaller
+    {
+        private readonly PotentiallyInlinedMethods potentiallyInlinedMethods;
+
+        public InlineTestCaller(PotentiallyInlinedMethods potentiallyInlinedMethods)
+        {
+            this.potentiallyInlinedMethods = potentiallyInlinedMethods;
+        }
+
+        public int AddNormal(int x, int y) => this.potentiallyInlinedMethods.AddNormal(x, y);
+
+        public int AddNoInline(int x, int y) => this.potentiallyInlinedMethods.AddNoInline(x, y);
+
+        public int AddAggressiveInline(int x, int y) =>
+            this.potentiallyInlinedMethods.AddAggressiveInline(x, y);
+    }
+
     public class InliningTests
     {
         public class CalculatorManager
@@ -36,16 +64,20 @@ namespace MockMe.Tests
             Assert.Equal(9999, calculatorManager.AddAggressiveInline(3, 5));
         }
 
-        //[Fact]
-        //public void CalculatorRandomNumber_ShouldReturnConfiguredValue()
-        //{
-        //    var calculatorMock = Mock.Me<Calculator>();
-        //    calculatorMock.Setup.Add(3, 5).Returns(9999, 9999, 9999);
-        //    CalculatorManager calculatorManager = new(calculatorMock);
+        [Fact]
+        public void AddMethod_ShouldReturnConfiguredValue()
+        {
+            var inlineMock = Mock.Me<PotentiallyInlinedMethods>();
 
-        //    Assert.Equal(9999, calculatorManager.AddNoInline(3, 5));
-        //    Assert.Equal(9999, calculatorManager.AddNormal(3, 5));
-        //    Assert.Equal(9999, calculatorManager.AddAggressiveInline(3, 5));
-        //}
+            inlineMock.Setup.AddNoInline(Arg.Any, Arg.Any).Returns(99);
+            inlineMock.Setup.AddNormal(Arg.Any, Arg.Any).Returns(999);
+            inlineMock.Setup.AddAggressiveInline(Arg.Any, Arg.Any).Returns(9999);
+
+            InlineTestCaller caller = new(inlineMock);
+
+            Assert.Equal(99, caller.AddNoInline(1, 1));
+            Assert.Equal(999, caller.AddNormal(1, 1));
+            Assert.Equal(9999, caller.AddAggressiveInline(1, 1));
+        }
     }
 }
