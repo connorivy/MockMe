c334999562201f3c98a838976da558d142fcf314
Connor Ivy <connor@speckle.systems>
2024-11-18 16:41:58 -0600

    add some inheritance tests

--- tests/MockMe.Tests/GenericMethodTests.cs
+++ tests/MockMe.Tests/GenericMethodTests.cs
@@ -6,28 +6,8 @@ namespace MockMe.Tests
 {
     public class GenericMethodTests
     {
-        //[Fact]
-        //public void GenericMethod_DefinedInDifferentAssembly_ShouldBeMockedSuccessfully()
-        //{
-        //    var ilReplacedObject = new MockMe.Tests.ExampleClasses.ComplexCalculator();
-        //    var randomReplaced = ilReplacedObject.AddUpAllOfThese2<double>(
-        //        0,
-        //        new double[] { 1, 2, 3, 4, 5 },
-        //        0
-        //    );
-
-        //    var ilDefinitionObject = new Calculator2();
-        //    var randomSource = ilDefinitionObject.AddUpAllOfThese2<double>(
-        //        0,
-        //        new double[] { 1, 2, 3, 4, 5 },
-        //        0
-        //    );
-
-        //    Assert.Equal(randomSource, randomReplaced);
-        //}
-
         [Fact]
-        public void Test()
+        public void GenericMethod_ShouldReturnConfiguredValue()
         {
             var mock = Mock.Me<ComplexCalculator>();
 
---
+++ tests/MockMe.Tests/InheritanceTests.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MockMe.Tests
+{
+    public class ParentClass
+    {
+        public virtual int Return99FromParent() => 99;
+
+        public virtual int Return55FromChildMethodOverride() => 99;
+    }
+
+    public class ChildClass : ParentClass
+    {
+        private int i1;
+        private int i2;
+        private int i3;
+
+        public override int Return99FromParent()
+        {
+            //simulate doing some work
+            i1 = Random.Shared.Next();
+            i2 = Random.Shared.Next();
+            i3 = Random.Shared.Next();
+
+            // call base class
+            return base.Return99FromParent();
+        }
+
+        public override int Return55FromChildMethodOverride()
+        {
+            //simulate doing some work
+            i1 = Random.Shared.Next();
+            i2 = Random.Shared.Next();
+            i3 = Random.Shared.Next();
+            var x = base.Return55FromChildMethodOverride();
+
+            return 55;
+        }
+    }
+
+    public class InheritanceTests
+    {
+        [Fact]
+        public void ChildClassWhichCallsBase_ShouldStillWork_WhenMocked()
+        {
+            var mock = Mock.Me<ChildClass>();
+
+            ChildClass notMocked = new();
+
+            Assert.Equal(99, notMocked.Return99FromParent());
+        }
+
+        [Fact]
+        public void ChildClassWhichOverridesBase_ShouldStillWork_WhenMocked()
+        {
+            var mock = Mock.Me<ChildClass>();
+
+            ChildClass notMocked = new();
+
+            Assert.Equal(55, notMocked.Return55FromChildMethodOverride());
+        }
+    }
+}
