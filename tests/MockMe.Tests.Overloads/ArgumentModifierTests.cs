ef5d309707a771f972ffdb8e222ae5e0860ef632
Connor Ivy <connor@speckle.systems>
2024-12-02 13:28:18 -0600

    add additional tests for setting out params

--- tests/MockMe.Tests.Overloads/ArgumentModifierTests.cs
+++ tests/MockMe.Tests.Overloads/ArgumentModifierTests.cs
@@ -5,7 +5,7 @@ namespace MockMe.Tests.Overloads;
 public class ArgumentModifierTests
 {
     [Fact]
-    public void OutKeyword()
+    public void OutKeyword_WhenConfiguredInReturnCall_ShouldSetTheCorrectValue()
     {
         var mock = Mock.Me<AllOverloads>(default(AllOverloads));
 
@@ -15,6 +15,21 @@ public class ArgumentModifierTests
                 args.arg = 55;
                 return 99;
             });
+
+        AllOverloads allOverloads = mock.MockedObject;
+        var result = allOverloads.OutArgument(out var outArg);
+
+        Assert.Equal(55, outArg);
+        Assert.Equal(99, result);
+        mock.Assert.OutArgument(out _).WasCalled();
+    }
+
+    [Fact]
+    public void OutKeyword_WhenConfiguredInCallbackCall_ShouldSetTheCorrectValue()
+    {
+        var mock = Mock.Me<AllOverloads>(default(AllOverloads));
+
+        mock.Setup.OutArgument(out _).Callback(args => args.arg = 55).Returns(99);
 
         AllOverloads allOverloads = mock.MockedObject;
         var result = allOverloads.OutArgument(out var outArg);
